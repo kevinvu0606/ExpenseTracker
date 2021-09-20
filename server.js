6631514f5407e1ae97c604363c0e5b25a5d018c4
Kevin <kevin.vu06@gmail.com>
2021-09-20 19:21:43 +1000

    heroku

--- server.js
+++ server.js
@@ -16,20 +16,20 @@ const transactions = require('./routes/transactions')
 const app = express()
 
 app.use(express.json());
-app.use(express.static('./client/build'))
 
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 
 }
 
+app.use('/api/v1/transactions', transactions)
+
+
+app.use(express.static('./client/build'))
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
+  res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
 })
 
-
-
-app.use('/api/v1/transactions', transactions)
 
 const PORT = process.env.PORT || 5000;
 
